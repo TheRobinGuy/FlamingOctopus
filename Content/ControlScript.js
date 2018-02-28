a987f6ecb41975f0d9119eb1d4564927bf930647
Glenn Gilmartin <gilmartin.glenn@gmail.com>
2018-02-28 23:38:38 +0000

    Ajax for DarkSky Started

--- Content/ControlScript.js
+++ Content/ControlScript.js
@@ -1,4 +1,13 @@
 "use strict"
 $(document).ready(function(){
-  console.log("Document Loaded");
+    console.log("Document Loaded");
+    
+    $.ajax({
+        url: 'https://api.darksky.net/forecast/da3446444c06189f7f6a7a9cb0727927/37.8267,-122.4233',
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+            alert(data);
+        }
+    });
 });
