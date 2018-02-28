5241d5dab87a866df1c40add2d0aa1f801ddcd1a
Glenn Gilmartin <gilmartin.glenn@gmail.com>
2018-02-28 23:45:35 +0000

    Proxy added to bypass CORS Errors
    No idea how !?

--- Content/ControlScript.js
+++ Content/ControlScript.js
@@ -1,13 +1,14 @@
 "use strict"
-$(document).ready(function(){
-    console.log("Document Loaded");
-    
-    $.ajax({
-        url: 'https://api.darksky.net/forecast/da3446444c06189f7f6a7a9cb0727927/37.8267,-122.4233',
-        type: "GET",
-        dataType: "json",
-        success: function(data) {
-            alert(data);
-        }
-    });
+$(document).ready(function() { 
+
+  var proxy = 'https://cors-anywhere.herokuapp.com/'; // -- This Proxy Somehow Gets Around CORS Errors !?!?!
+  var apiLinkDS = "https://api.darksky.net/forecast/da3446444c06189f7f6a7a9cb0727927/37.8267,-122.4233";
+
+  $.ajax({
+    url: proxy + apiLinkDS,
+    success:function(data) { 
+        console.log(data);
+    }
+  });
+
 });
