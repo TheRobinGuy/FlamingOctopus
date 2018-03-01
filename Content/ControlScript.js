693ff4c0d5b42bc997e01e5b64eae8c6d6b96c6b
Glenn Gilmartin <gilmartin.glenn@gmail.com>
2018-03-01 11:27:52 +0000

    Usage fro Google Maps
    Refresh on button click

--- Content/ControlScript.js
+++ Content/ControlScript.js
@@ -1,6 +1,5 @@
 "use strict"
 $(document).ready(function() { 
-
     $("#Submit").click(function(){
         var Lat = $("#LatitudeText").val();    
         var Long = $("#LongitudeText").val();
@@ -8,7 +7,7 @@ $(document).ready(function() {
         var proxy = "https://cors-anywhere.herokuapp.com/"; // -- This Proxy Somehow Gets Around CORS Errors !?!?!
         var apiLinkDS = "https://api.darksky.net/forecast/da3446444c06189f7f6a7a9cb0727927/" + Lat + "," + Long + "";
         //"https://api.darksky.net/forecast/da3446444c06189f7f6a7a9cb0727927/37.8267,-122.4233";
-
+        myMap(); //Execute Maps Refresh
           $.ajax({
             url: proxy + apiLinkDS,
             success:function(data) { 
