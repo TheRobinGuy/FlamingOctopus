b60a6c213dd19b7af688a854f454ffe020bc0ed7
Glenn Gilmartin <gilmartin.glenn@gmail.com>
2018-03-01 00:37:29 +0000

    Returns JSON Object for inputted Lat + Long
    Can get the weather forecast for the inputted location, and other details.

--- Content/ControlScript.js
+++ Content/ControlScript.js
@@ -1,14 +1,24 @@
 "use strict"
 $(document).ready(function() { 
 
-  var proxy = 'https://cors-anywhere.herokuapp.com/'; // -- This Proxy Somehow Gets Around CORS Errors !?!?!
-  var apiLinkDS = "https://api.darksky.net/forecast/da3446444c06189f7f6a7a9cb0727927/37.8267,-122.4233";
+    $("#Submit").click(function(){
+        var Lat = $("#LatitudeText").val();    
+        var Long = $("#LongitudeText").val();
+        console.log("Lat: " + Lat + " Long: " + Long);
+        var proxy = "https://cors-anywhere.herokuapp.com/"; // -- This Proxy Somehow Gets Around CORS Errors !?!?!
+        var apiLinkDS = "https://api.darksky.net/forecast/da3446444c06189f7f6a7a9cb0727927/" + Lat + "," + Long + "";
+        //"https://api.darksky.net/forecast/da3446444c06189f7f6a7a9cb0727927/37.8267,-122.4233";
 
-  $.ajax({
-    url: proxy + apiLinkDS,
-    success:function(data) { 
-        console.log(data);
-    }
-  });
-
+          $.ajax({
+            url: proxy + apiLinkDS,
+            success:function(data) { 
+                console.log(data);
+                var imgSrc = data.currently.icon;
+                var location = data.timezone.split('/')[1];
+                console.log(location);
+                
+                $("#output").text("There will be " + imgSrc + " in " + location + ".");
+            }
+          });
+    });
 });
